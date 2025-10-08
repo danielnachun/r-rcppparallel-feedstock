6e6a03dcca71b3edf52a7f43ed00579472df557e
Daniel Nachun <daniel.nachun@gmail.com>
2025-10-07 17:04:12 -0700

    patch makevars

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 set TBB_LIB=%LIBRARY_PREFIX%\lib
 set TBB_INC=%LIBRARY_PREFIX%\include
-sed -i 's?-Ltbb/build/lib_release??' src/Makevars.in
+sed -i 's?tbb/build/lib_release?%LIBRARY_PREFIX%\bin?' src/Makevars.in
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
