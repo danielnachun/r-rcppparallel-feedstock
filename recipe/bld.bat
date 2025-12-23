bbd8fe3965ee0b310cc078ed0cef2d0a8dde8888
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 21:04:39 -0600

    delete MSVC Clang

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,13 @@ IF %ERRORLEVEL% NEQ 0 exit /B 1
 sed -i -e "s/void R_init_RcppParallel/__declspec(dllexport) void R_init_RcppParallel/" src/init.cpp
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
-set PATH=%BUILD_PREFIX%\bin;%LIBRARY_PREFIX%\bin;%PREFIX%\bin;%PATH%
+if defined CI (
+    RMDIR /s /q "C:\Program Files\LLVM" || (Echo Ignoring failure to delete C:\Program Files\LLVM)
+    RMDIR /s /q "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm" ^
+        || (Echo Ignoring failure to delete C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm)
+    RMDIR /s /q "C:\Program Files\Microsoft Visual Studio\2026\Enterprise\VC\Tools\Llvm" ^
+        || (Echo Ignoring failure to delete C:\Program Files\Microsoft Visual Studio\2026\Enterprise\VC\Tools\Llvm)
+)
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
