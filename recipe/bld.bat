71269939b282dc9f393b7d2af4da8d6237fe4083
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 21:26:52 -0600

    fix typo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ set TBB_INC=%LIBRARY_PREFIX%\include
 sed -i 's?tbb/build/lib_release?%LIBRARY_PREFIX:\=/%/bin?' src/Makevars.in
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
-sed -i 's/PKG_CXXFLAGS = /PKG_CXXFLAGS = -DNOMINMAX/g' src/Makevars.in
+sed -i 's/PKG_CXXFLAGS = /PKG_CXXFLAGS = -DNOMINMAX /g' src/Makevars.in
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 sed -i -e "s/void R_init_RcppParallel/__declspec(dllexport) void R_init_RcppParallel/" src/init.cpp
