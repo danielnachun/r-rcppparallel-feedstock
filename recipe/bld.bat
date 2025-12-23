0cc791371c72c6d8b73b5e123e5498303877dbbe
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 20:29:01 -0600

    set CXXFLAG and correct PATH

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,11 +2,14 @@ set TBB_LIB=%LIBRARY_PREFIX%\lib
 set TBB_INC=%LIBRARY_PREFIX%\include
 sed -i 's?tbb/build/lib_release?%LIBRARY_PREFIX:\=/%/bin?' src/Makevars.in
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-sed -i 's?tbb/build/lib_release?%LIBRARY_PREFIX:\=/%/bin?' src/Makevars.in
+
+sed -i 's/PKG_CXXFLAGS = /PKG_CXXFLAGS = -DNOMINMAX/g' src/Makevars.in
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 sed -i -e "s/void R_init_RcppParallel/__declspec(dllexport) void R_init_RcppParallel/" src/init.cpp
 IF %ERRORLEVEL% NEQ 0 exit /B 1
+
+set PATH=%BUILD_PREFIX%\bin;%LIBRARY_PREFIX%\bin;%PREFIX%\bin;%PATH%
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
