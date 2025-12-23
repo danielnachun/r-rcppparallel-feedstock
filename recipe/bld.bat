e014872d8867c50d941ed131cf333591429e183e
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-23 12:21:12 -0600

    use simplified Makevars.ucrt

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,10 @@
 set TBB_LIB=%LIBRARY_PREFIX%\lib
 set TBB_INC=%LIBRARY_PREFIX%\include
-sed -i 's?tbb/build/lib_release?%LIBRARY_PREFIX:\=/%/bin?' src/Makevars.in
-IF %ERRORLEVEL% NEQ 0 exit /B 1
 
-sed -i 's/PKG_CXXFLAGS = /PKG_CXXFLAGS = -DNOMINMAX /g' src/Makevars.in
-IF %ERRORLEVEL% NEQ 0 exit /B 1
+copy %RECIPE_DIR%\Makevars.ucrt src
 
 sed -i -e "s/void R_init_RcppParallel/__declspec(dllexport) void R_init_RcppParallel/" src/init.cpp
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 "%R%" CMD INSTALL --build . %R_ARGS%
-IF %ERRORLEVEL% NEQ 0 type src\Makevars
+IF %ERRORLEVEL% NEQ 0 exit /B 1
